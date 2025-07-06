693006979b7f51bae914d6439d0648c03c7b3c0f
francescobodria <francesco.bodria95@gmail.com>
2025-07-06 10:02:42 +0200

    update hotspot script

--- script_trap_cam/hotspot.sh
+++ script_trap_cam/hotspot.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# create hotspot 
-sudo nmcli d wifi hotspot ifname wlan0 ssid TrapCam password <PASSWORD>
-# activate hotspot mode
-nmcli connection up Hotspot
-# deactivate hotspot mode
-nmcli connection down Hotspot
+# 1. Create a new Wi-Fi hotspot connection
+sudo nmcli connection add type wifi ifname wlan0 con-name TrapCam autoconnect yes ssid TrapCam
+
+# 2. Configure it to use 'ap' (access point) mode
+sudo nmcli connection modify TrapCam 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+
+# 3. Set a WPA2 password
+sudo nmcli connection modify TrapCam wifi-sec.key-mgmt wpa-psk
+sudo nmcli connection modify TrapCam wifi-sec.psk "TrapCam123"  # Must be 8+ characters
+
+# 4. Activate the hotspot
+sudo nmcli connection up TrapCam
